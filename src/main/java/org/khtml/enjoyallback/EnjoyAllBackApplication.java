5c6374b48733b0f948822b6fed560ef886a1122e
이윤영 <130331004+lydbsdud@users.noreply.github.com>
2024-08-12 15:42:14 +0900

    first commit

---
+++ src/main/java/org/khtml/enjoyallback/EnjoyAllBackApplication.java
@@ -0,0 +1,13 @@
+package org.khtml.enjoyallback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnjoyAllBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EnjoyAllBackApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/khtml/enjoyallback/EnjoyAllBackApplicationTests.java
@@ -0,0 +1,13 @@
+package org.khtml.enjoyallback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EnjoyAllBackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
