ac0ef74405c414123a7702281e6bdae5c11a3b40
rogi-rogi <yrkim6883@gmail.com>
2024-08-12 16:19:49 +0900

    init - Dockerfile 작성 - Security 설정 - CICD.yml 작성

---
+++ src/main/java/org/khtml/enjoyallback/config/SecurityConfig.java
@@ -0,0 +1,32 @@
+package org.khtml.enjoyallback.config;
+
+import lombok.RequiredArgsConstructor;
+import org.khtml.enjoyallback.config.jwt.JwtAuthenticationFilter;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class SecurityConfig {
+    private final JwtAuthenticationFilter jwtAuthenticationFilter;
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(request ->
+                        request
+                                .requestMatchers("/").permitAll()
+                                .requestMatchers("/api/auth/**").permitAll()
+                                .anyRequest().authenticated()
+                )
+                .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
+
+        return http.build();
+    }
+}
---
+++ src/main/java/org/khtml/enjoyallback/config/jwt/JwtTokenUtil.java
@@ -0,0 +1,76 @@
+package org.khtml.enjoyallback.config.jwt;
+
+
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import jakarta.annotation.PostConstruct;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import javax.crypto.spec.SecretKeySpec;
+import java.security.Key;
+import java.util.Date;
+
+@Component
+public class JwtTokenUtil {
+
+    @Value("${jwt.secret}")
+    private String SECRET_KEY;
+
+    @Value("${jwt.access-token-expiration}")
+    private long ACCESS_TOKEN_EXPIRY_TIME;
+
+    @Value("${jwt.refresh-token-expiration}")
+    private long REFRESH_TOKEN_EXPIRY_TIME;
+
+    private Key key;
+
+    @PostConstruct
+    public void inti() {
+        byte[] keyBytes = SECRET_KEY.getBytes();
+        this.key = new SecretKeySpec(keyBytes, SignatureAlgorithm.HS256.getJcaName());
+    }
+
+    public String createAccessToken(Long userId) {
+        return createToken(userId, ACCESS_TOKEN_EXPIRY_TIME);
+    }
+
+    public String createRefreshToken(Long userId) {
+        return createToken(userId, REFRESH_TOKEN_EXPIRY_TIME);
+    }
+
+    private String createToken(Long userId, long expiryTime) {
+        Date now = new Date();
+        Date expiration = new Date(now.getTime() + expiryTime);
+        return Jwts.builder()
+                .setSubject(userId.toString())
+                .setIssuedAt(now)
+                .setExpiration(expiration)
+                .signWith(key, SignatureAlgorithm.HS256)
+                .compact();
+    }
+
+    public boolean validateToken(String token, String userId) {
+        final String extractedUserId = extractUserId(token);
+        return (extractedUserId.equals(userId) && !isTokenExpired(token));
+    }
+
+    public boolean isTokenExpired(String token) {
+        return Jwts.parserBuilder()
+                .setSigningKey(key)
+                .build()
+                .parseClaimsJws(token)
+                .getBody()
+                .getExpiration()
+                .before(new Date());
+    }
+
+    public String extractUserId(String token) {
+        return Jwts.parserBuilder()
+                .setSigningKey(key)
+                .build()
+                .parseClaimsJws(token)
+                .getBody()
+                .getSubject();
+    }
+}
