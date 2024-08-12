1610c9a5ee3b6aa81e419709da06b4799d8619c4
rogi-rogi <yrkim6883@gmail.com>
2024-08-12 17:36:50 +0900

    issue - jwt filter 우회 오류 해결

--- src/main/java/org/khtml/enjoyallback/EnjoyAllBackApplication.java
+++ src/main/java/org/khtml/enjoyallback/EnjoyAllBackApplication.java
@@ -2,10 +2,10 @@ package org.khtml.enjoyallback;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
 @SpringBootApplication
 public class EnjoyAllBackApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(EnjoyAllBackApplication.class, args);
     }
--- src/main/java/org/khtml/enjoyallback/config/jwt/JwtAuthenticationFilter.java
+++ src/main/java/org/khtml/enjoyallback/config/jwt/JwtAuthenticationFilter.java
@@ -9,7 +9,7 @@ import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
 
-@Component
+//@Component
 public class JwtAuthenticationFilter extends OncePerRequestFilter {
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
